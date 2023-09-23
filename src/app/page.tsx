2303c544bb792b9c9265a38db885ef6ad32291ae
Jonathan Huergo <jh2000@hotmail.com.ar>
2023-09-23 19:20:04 -0300

    Define page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,46 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h1>Guardería Canina</h1>
+      <header>
+        <h1>Guardería Canina</h1>
+        <img src="" alt="" />
+      </header>
+      <section>
+        <div>
+          <h2>Texto Importante 1</h2>
+          <p>Texto</p>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h2>Texto Importante 1</h2>
+          <p>Texto</p>
+        </div>
+      </section>
+      <section>
+        <h2>Reseñas</h2>
+        <div>
+          <div>
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <h3>Nombre Cliente y Nombre Perro</h3>
+            <p>Texto reseña</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <h3>Nombre Cliente y Nombre Perro</h3>
+            <p>Texto reseña</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <h3>Nombre Cliente y Nombre Perro</h3>
+            <p>Texto reseña</p>
+          </div>
+        </div>
+        <img src="" alt="" />
+      </section>
     </main>
   )
 }
