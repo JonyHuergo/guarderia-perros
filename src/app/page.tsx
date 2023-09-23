6c05d4921c23c7ed7c406ff13d5890601febf6e5
Jonathan Huergo <jh2000@hotmail.com.ar>
2023-09-23 20:48:51 -0300

    Add Reviews component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,13 @@
 import Banner from '../components/Banner';
 import About from '../components/About';
+import Reviews from '../components/Reviews';
 
 export default function Home() {
   return (
     <main>
       <Banner/>
       <About/>
-      <section>
-        <h2>Reseñas</h2>
-        <div>
-          <div>
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <h3>Nombre Cliente y Nombre Perro</h3>
-            <p>Texto reseña</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <h3>Nombre Cliente y Nombre Perro</h3>
-            <p>Texto reseña</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <h3>Nombre Cliente y Nombre Perro</h3>
-            <p>Texto reseña</p>
-          </div>
-        </div>
-        <img src="" alt="" />
-      </section>
+      <Reviews/>
     </main>
   )
 }
---
+++ src/components/Reviews.js
@@ -0,0 +1,32 @@
+import styles from '../styles/Reviews.module.css'
+
+const Reviews = () => {
+    return (
+        <section className={styles.reviews}>
+            <h2>Reseñas</h2>
+            <div>
+            <div>
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <h3>Nombre Cliente y Nombre Perro</h3>
+                <p>Texto reseña</p>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <h3>Nombre Cliente y Nombre Perro</h3>
+                <p>Texto reseña</p>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <h3>Nombre Cliente y Nombre Perro</h3>
+                <p>Texto reseña</p>
+            </div>
+            </div>
+            <img src="" alt="" />
+        </section>
+    );
+}
+ 
+export default Reviews;
