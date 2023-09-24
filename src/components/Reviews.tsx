bbe17164e65d57301a2ec79a9a39ca0353e63b51
Jonathan Huergo <jh2000@hotmail.com.ar>
2023-09-24 19:09:13 -0300

    Add review photos

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -7,24 +7,24 @@ const Reviews = () => {
             <div className={styles.reviewsContainer}>
                 <div className={styles.review}>
                     <div className={styles.reviewPhotosContainer}>
-                        <img src="/white-dog.jpg" alt="" />
-                        <img src="/white-dog.jpg" alt="" />
+                        <img src="/person.jpg" alt="" />
+                        <img src="/dog.jpg" alt="" />
                     </div>
                     <h3 className={styles.reviewName}>Nombre Cliente y Nombre Perro</h3>
                     <p className={styles.reviewText}>Texto reseña</p>
                 </div>
                 <div className={styles.review}>
                     <div className={styles.reviewPhotosContainer}>
-                        <img src="/white-dog.jpg" alt="" />
-                        <img src="/white-dog.jpg" alt="" />
+                        <img src="/person2.jpg" alt="" />
+                        <img src="/dog2.jpg" alt="" />
                     </div>
                     <h3 className={styles.reviewName}>Nombre Cliente y Nombre Perro</h3>
                     <p className={styles.reviewText}>Texto reseña</p>
                 </div>
                 <div className={styles.review}>
                     <div className={styles.reviewPhotosContainer}>
-                        <img src="/white-dog.jpg" alt="" />
-                        <img src="/white-dog.jpg" alt="" />
+                        <img src="/person3.jpg" alt="" />
+                        <img src="/dog3.jpg" alt="" />
                     </div>
                     <h3 className={styles.reviewName}>Nombre Cliente y Nombre Perro</h3>
                     <p className={styles.reviewText}>Texto reseña</p>
